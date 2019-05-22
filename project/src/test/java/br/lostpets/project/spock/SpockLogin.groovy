7102013d5ca341c496313f4ecfe74b3af6728fab
Juvencio Jose <juvencio-ramalho@hotmail.com>
2019-05-21 21:05:37 -0300

    realizando spock login

---
+++ project/src/test/java/br/lostpets/project/spock/SpockLogin.groovy
@@ -0,0 +1,44 @@
+package br.lostpets.project.spock
+
+import javax.transaction.Transactional
+
+import org.jboss.jandex.TypeTarget.Usage
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+import br.lostpets.project.model.Usuario
+import br.lostpets.project.service.UsuarioService
+import spock.lang.*
+
+@Transactional
+@SpringBootTest
+class SpockLogin extends Specification{
+
+	private Usuario usuario; 
+	private Usuario usuario1;
+
+	@Autowired
+	UsuarioService usuarioService;
+	
+	def 'verificar se usuario consta na base de dados para acessar'() {
+		setup:
+			usuario = new Usuario("nome", "telefoneFixo", "telefoneCelular", "email", "senha", "idImagem", "cep", "rua", "bairro", "cidade", "uf", "latitude", "longitude")
+			usuarioService.salvarUsuario(usuario)
+		when:
+			usuario1 = usuarioService.emailSenha(usuario.getEmail(), usuario.getSenha())
+		then:
+			usuario1 != null
+	}
+	
+	def 'verificar se login ou senha é diferente de null'(){
+			given:
+				usuario.setEmail("email")
+				usuario.setSenha("senha")
+			expect:
+				usuario.getEmail() == usuario.getSenha()
+			
+	}
+	
+	"se colocou so "
+	
+}
