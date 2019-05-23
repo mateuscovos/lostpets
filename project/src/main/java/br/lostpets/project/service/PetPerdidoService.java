f5ed9a90e2a95e33effb6ddc04ad544d10852b5e
mateuscovos <mateus.covos@gmail.com>
2019-05-23 12:51:04 -0300

    testes spock PetPerdido

--- project/src/main/java/br/lostpets/project/service/PetPerdidoService.java
+++ project/src/main/java/br/lostpets/project/service/PetPerdidoService.java
@@ -12,27 +12,70 @@ import br.lostpets.project.repository.PetPerdidoRepository;
 @Service
 public class PetPerdidoService {
 
-	
 	@Autowired
 	PetPerdidoRepository petPerdidoRepository;
-	public List<PetPerdido> encontrarPetsAtivos(){
+
+	public List<PetPerdido> encontrarPetsAtivos() {
 		return petPerdidoRepository.getAtivos();
 	}
-	
+
 	public PetPerdido encontrarUnicoPet(int id) {
 		return petPerdidoRepository.getAtivosByIdAnimal(id);
 	}
+	
+	public PetPerdido encontrarUnicoPet(PetPerdido petPerdido) {
+		if(petPerdido == null) { return null; }
+		return petPerdidoRepository.getAtivosByIdAnimal(petPerdido.getIdAnimal());
+	}
 
-	public void salvarPet(PetPerdido petPerdido) {
-		if(petPerdido.getIdUsuario() != null) {
-			if(petPerdido.getIdUsuario().getIdPessoa() != 0) {
-				petPerdidoRepository.save(petPerdido);
-			}
-		}
+	public void salvarPet(PetPerdido petPerdido) {		
+		if (!this.isValidAttributes(petPerdido)) { return; }
+	
+		petPerdidoRepository.save(petPerdido);	
 	}
 
 	public List<PetPerdido> encontrarTodos() {
 		return petPerdidoRepository.findAll();
 	}
-	
+
+	private boolean isValidAttributes(PetPerdido petPerdido) {
+
+		if (petPerdido == null) { 
+			return false; 
+		}
+		if (petPerdido.getIdUsuario() == null) { 
+			return false; 
+		}		
+		if(petPerdido.getIdUsuario().getIdPessoa() == 0) {
+			return false; 
+		}
+		if (this.isEmptyOrNull(petPerdido.getDataPerdido())) {
+			return false; 
+		}
+		if (this.isEmptyOrNull(petPerdido.getDescricao())) {
+			return false; 
+		}
+		if (this.isEmptyOrNull(petPerdido.getNomeAnimal())) {
+			return false; 
+		}
+		if (this.isEmptyOrNull(petPerdido.getTipoAnimal())) {
+			return false; 
+		}
+		if (this.isEmptyOrNull(petPerdido.getCep())) {
+			return false; 
+		}
+
+		return true; 
+	}
+
+	private boolean isEmptyOrNull(String text) {
+		if (text == null) {
+			return true;
+		}
+		if (text.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
 }
--- project/src/test/java/br/lostpets/project/spock/PetPerdidoTest.groovy
+++ project/src/test/java/br/lostpets/project/spock/PetPerdidoTest.groovy
@@ -28,41 +28,27 @@ class PetPerdidoTest extends Specification {
 	PetPerdidoService petPerdidoService
 	@Autowired
 	UsuarioService usuarioService;
+	def usuario
+	def petPerdido
+	
+	def setup(){
+		usuario = new Usuario("mateus", "mateus@lost.com", "(11) 91234-1234", "(11) 1234-1234")
+		usuarioService.salvarUsuario(usuario)
+		petPerdido = new PetPerdido(usuario,"tobias", "12/12/2018", "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
+		petPerdidoService.salvarPet(petPerdido)
+	}
 	
 	def 'cadastra pet perdido COM Usuario'() {
 				
 		when:
-			def usuario = new Usuario("mateus", "mateus@lost.com", "(11) 91234-1234", "(11) 1234-1234")
-			usuarioService.salvarUsuario(usuario)			
-			def petPerdido = new PetPerdido(usuario,"tobias", "12/12/2018", "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
-			petPerdidoService.salvarPet(petPerdido)	
-			
 			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido.getIdAnimal())		
 		then:
 			petPerdido == pet
 	}
 	
-	def 'não permitir cadastrar pet perdido SEM Usuario'() {
-		
-		when:
-			def petPerdido = new PetPerdido(null,"tobias", "12/12/2018", "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
-			petPerdidoService.salvarPet(petPerdido)
-			
-			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido.getIdAnimal())
-		then:
-			pet == null
-	}
-	
 	def 'pegar todos animais perdidos ativos'() {
 		
 		when:
-			
-			def usuario = new Usuario("mateus", "mateus@lost.com", "(11) 91234-1234", "(11) 1234-1234")
-			usuarioService.salvarUsuario(usuario)
-		
-			def petPerdido = new PetPerdido(usuario,"tobias", "12/12/2018", "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
-			petPerdidoService.salvarPet(petPerdido)
-			
 			def petPerdido2 = new PetPerdido(usuario,"murilo", "12/12/2018", "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
 			petPerdidoService.salvarPet(petPerdido2)
 		
@@ -72,7 +58,78 @@ class PetPerdidoTest extends Specification {
 			list.size() == 2
 	}
 	
+	def 'não permitir cadastrar pet perdido NULL'() {
+		
+		when:
+			PetPerdido petPerdido_ = null
+			petPerdidoService.salvarPet(petPerdido_)
+			
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido_)
+		then:
+			pet == null
+	}
 	
+	def 'não permitir cadastrar pet perdido SEM Usuario'() {
+		
+		when:
+			def petPerdidoSemUsuario = new PetPerdido(null,"tobias", "12/12/2018", "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
+			petPerdidoService.salvarPet(petPerdidoSemUsuario)
+			
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdidoSemUsuario.getIdAnimal())
+		then:
+			pet == null
+	}
+	
+	
+	def 'não permitir cadastrar pet perdido SEM a data de desaparecido'(){
+		when:
+			def petPerdido_ = new PetPerdido(usuario,"tobias", null, "Descrição perdido","Gato","C://Path","00.000.000","Latitude","Longitude")
+			petPerdidoService.salvarPet(petPerdido_)
+		
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido_.getIdAnimal())
+		then:
+			pet == null;
+	}
+	
+	def 'não permitir cadastrar pet perdido SEM descrição do sumiço'(){
+		when:
+			def petPerdido_ = new PetPerdido(usuario,"tobias", "12/12/2018", null,"Gato","C://Path","00.000.000","Latitude","Longitude")
+			petPerdidoService.salvarPet(petPerdido_)
+		
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido_.getIdAnimal())
+		then:
+			pet == null;
+	}
+	
+	def 'não permitir cadastrar pet perdido SEM nome'(){
+		when:
+			def petPerdido_ = new PetPerdido(usuario,null, "12/12/2018", "Descrição do desaparecimento","Gato","C://Path","00.000.000","Latitude","Longitude")
+			petPerdidoService.salvarPet(petPerdido_)
+		
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido_.getIdAnimal())
+		then:
+			pet == null;
+	}
+	
+	def 'não permitir cadastrar pet perdido SEM categoria'(){
+		when:
+			def petPerdido_ = new PetPerdido(usuario,"tobias", "12/12/2018", "Descrição do desaparecimento",null,"C://Path","00.000.000","Latitude","Longitude")
+			petPerdidoService.salvarPet(petPerdido_)
+		
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido_.getIdAnimal())
+		then:
+			pet == null;
+	}
+	
+	def 'não permitir cadastrar pet perdido SEM cep'(){
+		when:
+			def petPerdido_ = new PetPerdido(usuario,"tobias", "12/12/2018", "Descrição do desaparecimento","Gatp","C://Path",null,"Latitude","Longitude")
+			petPerdidoService.salvarPet(petPerdido_)
+		
+			def PetPerdido pet = petPerdidoService.encontrarUnicoPet(petPerdido_.getIdAnimal())
+		then:
+			pet == null;
+	}
 	
 	
 }
